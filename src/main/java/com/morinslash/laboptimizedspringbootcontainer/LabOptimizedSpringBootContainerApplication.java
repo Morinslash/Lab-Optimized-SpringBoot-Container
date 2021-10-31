4f45cf139111a988696f258adb9fd92a44266bc4
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2021-10-31 17:07:28 +0100

    Initial git commit with generate Spring Boot application

---
+++ src/main/java/com/morinslash/laboptimizedspringbootcontainer/LabOptimizedSpringBootContainerApplication.java
@@ -0,0 +1,13 @@
+package com.morinslash.laboptimizedspringbootcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabOptimizedSpringBootContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabOptimizedSpringBootContainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morinslash/laboptimizedspringbootcontainer/LabOptimizedSpringBootContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morinslash.laboptimizedspringbootcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOptimizedSpringBootContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
